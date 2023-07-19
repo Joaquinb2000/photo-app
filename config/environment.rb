7dd486bb7c854ec474673cd4b290de79c75c64af
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-07-19 02:35:21 -0400

    update production config

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
   :password => ENV['SENDGRID_PASSWORD'], # This is the secret sendgrid API key which was issued during API key creation
-  :domain => 'fly.io',
+  :domain => 'photo-app-jb.fly.dev',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
